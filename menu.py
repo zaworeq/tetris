cfdcbf2ff58f597376c26807c4ceee643a09259d
zaworeq <zaworskiwiktor570@gmail.com>
2021-11-17 11:55:21 +0100

    git add .

--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import Image, ImageTk
 from game import new_instance
 
 
@@ -9,13 +10,17 @@ def new_game():
 
 menu = Tk()
 menu.title("Tetrisik")
-menu.geometry("200x150")
-menu.config(bg= "light green")
 
-start_btn = Button(menu, text="New game", font=("Times New Roman",20), command= new_game)
-start_btn.pack()
+canvas = Canvas(menu)
+canvas.grid(row=0, column=0, rowspan=10, columnspan=11)
 
-quit_btn = Button(menu, text="Quit", font=("Times New Roman",20), command= quit)
-quit_btn.pack()
+bg_image = ImageTk.PhotoImage(Image.open('menu_bg.jpg'))
+menu_bg = canvas.create_image(0, 0, image=bg_image, anchor=NW)
+
+start_btn = Button(menu, text="New game", font=("Times New Roman", 20), command=new_game)
+start_btn.grid(row=8, column=5)
+
+quit_btn = Button(menu, text="Quit", font=("Times New Roman", 20), command=quit)
+quit_btn.grid(row=9, column=5)
 
 menu.mainloop()
